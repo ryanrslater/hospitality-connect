284259f0aca03bee399c26bd1d0915057f60e471
Ryan Slater <ryanryanslater@gmail.com>
2022-11-11 23:10:59 +1000

    Auth error

--- lib/Auth.ts
+++ lib/Auth.ts
@@ -2,14 +2,15 @@ import { PrismaClient, Account } from '@prisma/client'
 import { unstable_getServerSession } from "next-auth/next"
 import { authOptions } from "../pages/api/auth/[...nextauth]"
 import type { NextApiRequest, NextApiResponse } from 'next'
-const prisma = new PrismaClient()
 
 export class Auth {
+    private prisma = new PrismaClient()
+
     async authenticateUser(req: NextApiRequest, res: NextApiResponse): Promise<Account> {
         const session = await unstable_getServerSession(req, res, authOptions)
         if (session) {
-            const prisma = new PrismaClient()
-            const account: Account | null = await prisma.account.findFirst({
+
+            const account: Account | null = await this.prisma.account.findFirst({
                 where: {
                     email: session.user?.email?.toString()
                 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,16 @@
 import NextAuth, {NextAuthOptions} from "next-auth"
 import CognitoProvider from "next-auth/providers/cognito";
 import { PrismaClient, Account } from '@prisma/client'
+
+const {COGNITO_CLIENT_ID, COGNITO_CLIENT_SECRET, COGNITO_ISSUER} = process.env
+
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     CognitoProvider({
-        clientId: process.env.COGNITO_CLIENT_ID,
-        clientSecret: process.env.COGNITO_CLIENT_SECRET,
-        issuer: process.env.COGNITO_ISSUER,
+        clientId: COGNITO_CLIENT_ID ? COGNITO_CLIENT_ID : "",
+        clientSecret: COGNITO_CLIENT_SECRET ? COGNITO_CLIENT_SECRET : '',
+        issuer: COGNITO_ISSUER,
       })
     // ...add more providers here
   ]
