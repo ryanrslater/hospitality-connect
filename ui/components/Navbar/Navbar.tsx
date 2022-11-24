34f9ac72b2ef3e43deca9b25dc6f175ebe57ae85
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-11-24 11:11:59 +0000

    Navbar styling

--- ui/components/Navbar/Navbar.tsx
+++ ui/components/Navbar/Navbar.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import styles from './Navbar.module.css'
 const Navbar = () => {
-    return <div>Navbar</div>
+    return (
+        <div className={styles.wrapper}>
+            <div>
+                Navbar
+            </div>
+            <div className={styles.links}>
+                <p>link</p>
+                <p>link</p>
+                <p>link</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
