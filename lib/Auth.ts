74e06018ea54b8f6e30a14a48d596b6930dba807
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-11-27 02:23:13 +0000

    auth

--- lib/Auth.ts
+++ lib/Auth.ts
@@ -1,5 +1,5 @@
 import { User } from "next-auth"
-import { unstable_getServerSession } from "next-auth/next"
+import session, { unstable_getServerSession } from "next-auth/next"
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { authOptions } from '../pages/api/auth/[...nextauth]'
 import { PrismaClient, Account, Prisma } from '@prisma/client'
@@ -9,7 +9,7 @@ import {
     CognitoIdentityProvider,
     InitiateAuthCommandInput,
     SignUpCommandInput,
-    ConfirmSignUpRequest, SignUpCommandOutput,
+    ConfirmSignUpRequest, SignUpCommandOutput, InitiateAuthCommandOutput,
     ConfirmSignUpCommandOutput, CognitoIdentityProviderClientConfig
 } from '@aws-sdk/client-cognito-identity-provider'
 
@@ -28,11 +28,11 @@ export class Auth {
         if (!process.env.SECRET_ACCESS_KEY) throw Error('missing secret key')
 
         this.client = new CognitoIdentityProvider({
-            region: this.region, 
+            region: this.region,
             credentials: {
                 accessKeyId: process.env.ACCESS_KEY_ID,
                 secretAccessKey: process.env.SECRET_ACCESS_KEY,
-                
+
             }
         })
     }
@@ -44,31 +44,21 @@ export class Auth {
         const req: InitiateAuthCommandInput = {
             AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
             ClientId: this.clientId,
-      
+
             AuthParameters: {
                 USERNAME: credentials.username,
                 PASSWORD: credentials.password
             }
         }
 
-        await this.client.initiateAuth(req)
-
-        const db = await this.prisma.account.findFirst({
-            where: {
-                username: credentials.username
-            }
-        })
-
-        if (!db) throw Error()
-
-        const user: User = {
-            id: db.email,
-            name: db.name,
-            email: db.email,
-            image: db.profileImage
-
+        const auth: InitiateAuthCommandOutput = await this.client.initiateAuth(req)
+        
+        const res: User = {
+            id: '1',
+            
         }
-        return user
+
+        return res
     }
 
     async signUp(username: string | undefined, email: string | undefined, password: string | undefined, confirmPassword: string | undefined): Promise<SignUpCommandOutput> {
@@ -80,12 +70,12 @@ export class Auth {
 
         const req: SignUpCommandInput = {
             ClientId: this.clientId,
-            UserAttributes: [ 
-                { 
-                   "Name": "email",
-                   "Value": email
+            UserAttributes: [
+                {
+                    "Name": "email",
+                    "Value": email
                 }
-             ],
+            ],
             Username: username,
             Password: password
         }
@@ -94,26 +84,20 @@ export class Auth {
         console.log(cognito)
         if (!cognito.UserSub) throw Error()
 
+     
+
         return cognito
     }
 
-    async authenticate(req: NextApiRequest, res: NextApiResponse): Promise<Account> {
+    async authenticate(req: NextApiRequest, res: NextApiResponse): Promise<typeof session> {
         const session = await unstable_getServerSession(req, res, authOptions)
 
         if (!session || !session.user) throw Error()
 
         if (!session.user.email) throw Error()
 
-        const prisma = new PrismaClient()
 
-        const db = await prisma.account.findFirst({
-            where: {
-                email: session.user.email
-            }
-        })
-        if (!db) throw Error()
-
-        return db
+        return session
     }
 
     async confirmAccount(username: string, code: string): Promise<ConfirmSignUpCommandOutput> {
