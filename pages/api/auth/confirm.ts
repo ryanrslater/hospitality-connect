69535724fdc7a0ce27bc59a4b246f0178a432f6d
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2022-12-12 11:54:59 +0000

    updated package

--- pages/api/auth/confirm.ts
+++ pages/api/auth/confirm.ts
@@ -1,22 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Auth } from '../../../lib/Auth'
-import { Account } from '../../../lib/Account'
+import NextCognito from 'nextcognito'
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const auth = new Auth()
-    const acc = new Account()
+    const nextcognito = new NextCognito()
     const body = JSON.parse(req.body)
 
-    const user = await auth.confirmAccount(body.username, body.code)
+    const user = await nextcognito.confirmSignUp(body.username, body.code)
 
-    if (user.error) return res.status(400).json({ error: user.error })
 
-    if (!user.sub) return res.status(200).json({ error: 'John Doe' })
-    
-    const account = await acc.create(body.username, user.sub, body.email,)
 
     res.status(200).json({ success: true })
     return res
--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -7,14 +7,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  //  const auth = new Auth()
-  //  const body = JSON.parse(req.body)
-  
-  //   const user = await auth.signIn(body.username, body.password, res) 
+
+   const body = JSON.parse(req.body)
 
   const auth = new NextCognito()
 
-  const user = auth.initiateAuth()
+  const user = auth.signIn(body.username, body.password, res)
+
+
   
   res.status(200).json({ name: 'John Doe' })
   return res
